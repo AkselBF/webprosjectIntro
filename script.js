0d7f4a548196748b963a4c2a854267a220a56ed7
AkselBF <aksel.boutrouefaret@yahoo.com>
2019-06-02 15:48:56 +0200

    Script

---
+++ script.js
@@ -0,0 +1,4 @@
+// to manipulate the text in html
+document.querySelector("title").innerHTML = "Start";
+document.querySelector("h1").innerHTML = "Continue";
+document.querySelector("h2").innerHTML = "We are Vivify, five not-so-great students ready to make a blast.";
